1874b5cef937d185571a2ecd011325712ed0bc2f
soncahuynh <huynhsonca@gmail.com>
2017-02-21 01:42:20 +0700

    second commit

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,6 +10,22 @@ class UsersController extends AppController
     var $component = array("Session");
     public $uses = array('Users');
 
+    public function initialize()
+    {
+        $this->loadComponent('Flash');
+        $this->loadComponent('Auth', [
+            'loginRedirect' => [
+                'controller' => 'Index',
+                'action' => 'index'
+            ],
+            'logoutRedirect' => [
+                'controller' => 'Index',
+                'action' => 'index',
+                'home'
+            ]
+        ]);
+    }
+
     /**
      * dang nhap he thong
      */
@@ -17,14 +33,20 @@ class UsersController extends AppController
     {
         $error = "";
         if ($this->request->is('post')) {
-            $username = $this->request->data('username');
+           /* $username = $this->request->data('username');
             $password = $this->request->data('password');
             if ($this->Users->checkLogin($username, $password)) {
                 $this->Session->write("Session", $username); //ghi session
                 $this->redirect("edit");//đăng nhập thành công chuyển trang thông tin
             } else {
                 $error = "Tên đăng nhập và mật khẩu không đúng";
+            }*/
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
             }
+            $this->Flash->error(__('Invalid username or password, try again'));
         }
         $this->set("error", $error);
     }
--- src/Template/Users/login.ctp
+++ src/Template/Users/login.ctp
@@ -1,9 +1,16 @@
-<form method="post" action="">
-   <fieldset>
-     <legend>Đăng nhập</legend>
-     <label>Tên đăng nhập</label><input type="text" name="username" /><br />
-     <label>Mật khẩu</label><input type="password" name="password" /><br />
-     <input type="submit" name="ok" value="Login" />
-     <span class="error"><?php echo $error; ?></span>
-   </fieldset>
-</form>
+<div class="col-lg-4">
+    <?php echo $this->Form->create('Form') ?>
+    <fieldset>
+        <legend>Đăng nhập</legend>
+        <div class="form-group">
+            <label for="username">Tên đăng nhập</label>
+            <?php echo $this->Form->input('username', ['type' => 'text', 'class' => 'form-control']) ?>
+        </div>
+        <div class="form-group">
+            <label for="password">Mật khẩu</label>
+            <?php echo $this->Form->input('password', ['type' => 'password', 'class' => 'form-control']) ?>
+        </div>
+        <?php echo $this->Form->submit('Login', ['class' => 'btn btn-primary']) ?>
+    </fieldset>
+    <?php echo $this->Form->end() ?>
+</div>
